e99e043cace577dda6715fcbdba05d979d2df8c4
akiyowind <atran059@ucr.edu>
2019-06-05 12:50:25 -0700

    Fixed navbar nav offset

--- js/main.js
+++ js/main.js
@@ -1,9 +1,24 @@
-$(document).ready(function() {
-    setTimeout(function() {
-        $('body').addClass('loaded');
-        setTimeout(function(){
-            $('#loader-wrapper').remove();
-        }, 3000);
-    }, 3000);
-});
+var divID;
 
+jQuery(document).ready(function($) {
+    //----- Preloader -----
+
+    $(document).ready(function() {
+        setTimeout(function() {
+            $('body').addClass('loaded');
+            setTimeout(function(){
+                $('#loader-wrapper').remove();
+            }, 3000);
+        }, 3000);
+    });
+
+    // ----- Navbar offset attempt #5000 -----
+
+    $('.nav-link').click(function(){
+        divId = $(this).attr('href');
+        $('html, body').animate({
+            scrollTop: $(divId).offset().top - 50
+        }, 100);
+    })
+
+});
